d408010fbd94b03d4311f1957ae5096adc81410d
=OnkLabs <onklabs@gmail.com>
2025-06-02 20:14:42 +0700

    cors

--- api/signaling.js
+++ api/signaling.js
@@ -5,6 +5,10 @@ import { kv } from '@vercel/kv';
 const MATCH_TTL = 300;
 
 export default async function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
   const method = req.method;
   const { userId, matchId, type, payload } = req.body || req.query || {};
 
